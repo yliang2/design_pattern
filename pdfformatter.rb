4f072ab4dd1c53658fbce1dc3361604c71ccfd8b
Sam Joseph <tansaku@gmail.com>
2013-12-12 10:05:19 +0000

    fixed up example to output stuff

--- pdfformatter.rb
+++ pdfformatter.rb
@@ -10,28 +10,36 @@ class PdfWithPasswordFormatter < PdfFormatter
 	 @base = base 
   end
   def protect_with_password
-      puts "this is a pdf with password test"
+    return "this is a pdf with password test\n"
   end
   def output
-    @base.output.protect_with_password
+    @base.output.to_s + protect_with_password
   end
 end
 
 class PdfWithWatermarkFormatter < PdfFormatter
   def initialize(base)
-	 @base = base 
+	  @base = base
   end
   def add_watermark
-	 puts "this is a pdf with water mark test"
-   end
+	  return "this is a pdf with water mark test\n"
+    end
   def output
-   @base.output.add_watermark
+    @base.output.to_s + add_watermark
   end
 end
+
 # If we just want a plain PDF
 formatter = PdfFormatter.new
+
+puts formatter.output
+
 # If we want a "draft" watermark
 formatter = PdfWithWatermarkFormatter.new(PdfFormatter.new)
+
+puts formatter.output
+
 # Both password protection and watermark
-formatter = PdfWithWatermarkFormatter.new(
-PdfWithPasswordFormatter.new(PdfFormatter.new))
+formatter = PdfWithWatermarkFormatter.new(PdfWithPasswordFormatter.new(PdfFormatter.new))
+
+puts formatter.output
